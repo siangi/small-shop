c4bf359c21b10e28f5b63a6a0b9ad8b1fe1d6ac8
siangi <siangi@gmx.ch>
2022-04-29 09:22:33 +0200

    init components

---
+++ src/components/BasketItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BasketItem() {
+    return <div>BasketItem</div>;
+}
+
+export default BasketItem;
---
+++ src/components/Checkoutform.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Checkoutform() {
+    return <div>Checkoutform</div>;
+}
+
+export default Checkoutform;
