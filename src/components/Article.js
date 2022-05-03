a23cbe8c973d4afb2bd0eb9e7580a04b7141e1c9
siangi <siangi@gmx.ch>
2022-05-03 09:54:34 +0200

    added imges

--- src/components/Article.js
+++ src/components/Article.js
@@ -9,6 +9,7 @@ function Article(props) {
     return (
         <li className="article">
             <article>
+                <img src={`https://kea-alt-del.dk/t7/images/webp/640/${props.id}.webp`} alt={"image of " + props.productdisplayname}></img>
                 <h3>{props.productdisplayname}</h3>
                 <p>{props.price} Kr</p>
                 {props.addToBasket != null ? (
